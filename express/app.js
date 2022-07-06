2256990c01e32b260b02c7a10e11c51ccfbe0d01
Georgesui <gdimov445@gmail.com>
2022-07-06 18:10:52 +0300

    final commit

---
+++ express/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var url = require('url')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+function checkAccess(req) {
+	return url.parse(req.url, true).query.secret === 'true'
+}
+
+app.use(function (req, res, next) {
+	if (req.url == '/home') {
+		res.end("Home");
+	} else {
+		next();
+	}
+});
+
+app.use(function (req, res, next) {
+	if (url.parse(req.url).pathname == '/forbidden' && checkAccess(req)) {
+		res.end("Access approved");
+	} else {
+		res.end("Access denied");
+	}
+});
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+module.exports = app;
